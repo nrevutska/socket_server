e8a0e6be804c6b5fb8cfba6a975915b0842b9a0e
nrevutska <nrevutska@gmail.com>
2019-01-09 11:21:30 +0200

    server part

---
+++ src/com/sockets/Server.java
@@ -0,0 +1,28 @@
+package com.sockets;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) {
+        int port = 5555;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket socket = serverSocket.accept();  // awaits for a connection with client
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            String line;
+            while (socket.isConnected()) {
+                line = inputStream.readUTF();
+                System.out.println("received" + line);
+                outputStream.writeUTF(line);
+                outputStream.flush();
+            }
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+    }
+}
